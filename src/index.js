b5f333bff88366743d58fcca55d4e38d984db73f
Karim Alibhai <karim@alibhai.co>
2019-06-28 01:59:56 -0400

    Add: exports from perf

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@
  * @copyright 2019-present Karim Alibhai. All rights reserved.
  */
 
-import './perf'
-
 import * as http from './server'
 import * as Config from './config'
 import * as Redis from './redis'
 
 export { http, Config, Redis }
-
+export * from './perf'
 export * from './logger'
